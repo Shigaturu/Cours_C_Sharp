e846645a4c6e1019eabf2d6d1744fa24252d3471
Fraisse Brian <fraissebrian47@gmail.com>
2023-10-06 11:36:02 +0200

    Ajoutez des fichiers projet.

---
+++ Cours C#/Program.cs
@@ -0,0 +1,33 @@
+﻿int nbFacture;
+    try
+    {
+        Console.WriteLine("Entrez le nombre de factures");
+        nbFacture = Convert.ToInt32(Console.ReadLine());
+    }
+    catch
+    {
+        Console.WriteLine("Saisie incorect appuyez sur une touche pour quitter");
+        throw;
+
+    }
+
+
+
+float i = 1;
+float sommeFacture = 0;
+while (i <= nbFacture)
+{
+    Console.WriteLine("Quel est le montant de la facture " + i);
+    float prixFacture = Convert.ToInt32(Console.ReadLine());
+    sommeFacture += prixFacture;
+    i++;
+}
+Console.WriteLine("Somme des factures en brut annuel " + sommeFacture + "€");
+Console.WriteLine("Somme des factures en net annuel " + sommeFacture * 0.75 + "€");
+Console.WriteLine("Facture par mois net " + (sommeFacture * 0.75) / 12 + "€");
+Console.WriteLine("Facture par mois brut " + sommeFacture / 12 + "€");
+
+if (sommeFacture > 36800)
+{
+    Console.WriteLine("Le plafond brut est dépassé de " + (sommeFacture - 36800) + "€");
+}
