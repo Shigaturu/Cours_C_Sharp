30445260e5931398492b5172efd91301286d5ae0
Fraisse Brian <fraissebrian47@gmail.com>
2023-10-06 13:19:20 +0200

    Ajout Try Catch Loop

--- Cours C#/Program.cs
+++ Cours C#/Program.cs
@@ -1,27 +1,47 @@
-﻿int nbFacture;
+﻿int nbFacture = 0;
+bool done = false;
+while (!done)
+{
     try
     {
         Console.WriteLine("Entrez le nombre de factures");
         nbFacture = Convert.ToInt32(Console.ReadLine());
+        done = true;
     }
     catch
     {
-        Console.WriteLine("Saisie incorect appuyez sur une touche pour quitter");
-        throw;
+        Console.WriteLine("Saisie incorect");
 
     }
+}
+
 
 
 
 float i = 1;
 float sommeFacture = 0;
-while (i <= nbFacture)
+bool done2 = false;
+while (!done2)
 {
-    Console.WriteLine("Quel est le montant de la facture " + i);
-    float prixFacture = Convert.ToInt32(Console.ReadLine());
-    sommeFacture += prixFacture;
-    i++;
+    try
+    {
+        while (i <= nbFacture)
+        {
+            Console.WriteLine("Quel est le montant de la facture " + i);
+            float prixFacture = Convert.ToInt32(Console.ReadLine());
+            sommeFacture += prixFacture;
+            i++;
+        }
+        done2 = true;
+    }
+
+    catch
+    {
+        Console.WriteLine("Saisie incorect");
+
+    }
 }
+
 Console.WriteLine("Somme des factures en brut annuel " + sommeFacture + "€");
 Console.WriteLine("Somme des factures en net annuel " + sommeFacture * 0.75 + "€");
 Console.WriteLine("Facture par mois net " + (sommeFacture * 0.75) / 12 + "€");
