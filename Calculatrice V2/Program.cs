841f25e68c7726150b57816134988703b7f61793
Fraisse Brian <fraissebrian47@gmail.com>
2023-10-11 08:46:37 +0200

    ajouts calculatrice

---
+++ Calculatrice V2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+bool done2 = false;
+while (!done2)
+{
+    try
+    {
+        Console.WriteLine("Saisir l'opération ");
+        string operation = Console.ReadLine();
+
+        // Utilisez Regex pour trouver tous les opérateurs dans la chaîne
+        MatchCollection operatorMatches = Regex.Matches(operation, @"[\+\-\*\/]");
+
+        // Créez un tableau pour stocker les opérateurs
+        string[] operators = new string[operatorMatches.Count];
+
+        // Parcourez les correspondances et ajoutez-les au tableau
+        for (int i = 0; i < operatorMatches.Count; i++)
+        {
+            operators[i] = operatorMatches[i].Value;
+        }
+
+        // Utilisez Regex pour diviser la chaîne en nombres
+        string[] numbers = Regex.Split(operation, @"[\+\-\*\/]");
+
+        // Convertissez les nombres en int
+        int[] numbersInt = numbers.Select(int.Parse).ToArray();
+
+        // Traitez le premier nombre
+        int result = numbersInt[0];
+
+        // Parcourez les opérateurs et effectuez les calculs
+        for (int i = 0; i < operators.Length; i++)
+        {
+            int nextNumber = numbersInt[i + 1];
+            string currentOperator = operators[i];
+
+            // Effectuez le calcul en fonction de l'opérateur
+            switch (currentOperator)
+            {
+                case "+":
+                    result += nextNumber;
+                    break;
+                case "-":
+                    result -= nextNumber;
+                    break;
+                case "*":
+                    result *= nextNumber;
+                    break;
+                case "/":
+                    if (nextNumber != 0) // Vérifiez la division par zéro
+                        result /= nextNumber;
+                    else
+                        Console.WriteLine("Division par zéro n'est pas autorisée.");
+                    break;
+                default:
+                    Console.WriteLine("Opérateur non pris en charge.");
+                    break;
+            }
+        }
+
+        // Affichez le résultat
+        Console.WriteLine("Résultat : " + result);
+
+        done2 = true;
+    }
+    catch
+    {
+        Console.WriteLine("Saisie incorrecte");
+    }
+}
--- Cours C#/Program.cs
+++ Cours C#/Program.cs
@@ -1,36 +1,19 @@
-﻿int nbFacture = 0;
-bool done = false;
-while (!done)
-{
-    try
-    {
-        Console.WriteLine("Entrez le nombre de factures");
-        nbFacture = Convert.ToInt32(Console.ReadLine());
-        done = true;
-    }
-    catch
-    {
-        Console.WriteLine("Saisie incorect");
-
-    }
-}
-
-
-
-
-float i = 1;
+﻿const float _plafondTTC = 36800;
 float sommeFacture = 0;
 bool done2 = false;
+
+float nbFacture = getNumberFromUser();
+
 while (!done2)
 {
     try
     {
-        while (i <= nbFacture)
+      for(int i = 0; i < nbFacture; i++)
         {
             Console.WriteLine("Quel est le montant de la facture " + i);
             float prixFacture = Convert.ToInt32(Console.ReadLine());
             sommeFacture += prixFacture;
-            i++;
+            
         }
         done2 = true;
     }
@@ -43,11 +26,45 @@ while (!done2)
 }
 
 Console.WriteLine("Somme des factures en brut annuel " + sommeFacture + "€");
-Console.WriteLine("Somme des factures en net annuel " + sommeFacture * 0.75 + "€");
-Console.WriteLine("Facture par mois net " + (sommeFacture * 0.75) / 12 + "€");
-Console.WriteLine("Facture par mois brut " + sommeFacture / 12 + "€");
+Console.WriteLine("Somme des factures en net annuel " + getNet(sommeFacture) + "€");
+Console.WriteLine("Facture par mois net " + getMensuel(getNet(sommeFacture)) + "€");
+Console.WriteLine("Facture par mois brut " + getMensuel(sommeFacture) + "€");
 
-if (sommeFacture > 36800)
+if (sommeFacture > _plafondTTC)
 {
-    Console.WriteLine("Le plafond brut est dépassé de " + (sommeFacture - 36800) + "€");
+    Console.WriteLine("Le plafond brut est dépassé de " + (sommeFacture - _plafondTTC) + "€");
+}
+
+
+float getNet(float brut)
+{
+    return brut * (float)0.75;
+}
+
+float getMensuel(float annuel)
+{
+    return annuel / (float)12;
+}
+
+float getNumberFromUser()
+{
+    float nbFacture = 0;
+    bool done = false;
+
+    while (!done)
+    {
+        try
+        {
+            Console.WriteLine("Entrez le nombre de factures");
+            nbFacture = Convert.ToInt32(Console.ReadLine());
+            done = true;
+
+        }
+        catch
+        {
+            Console.WriteLine("Saisie incorect");
+
+        }
+    }
+    return nbFacture;
 }
